27a9b6f06b3c9c33473d881605a397425b7cdcc7
Alvin McNair <alvin.mcnair@gmail.com>
2019-12-06 10:03:16 -0500

    Modified for advent 5
    Added 4 instructions
    Added Immediate mode to add and multiply
    Added error return for halt
    Added errors for instructions that fail to parse

--- main.go
+++ main.go
@@ -6,78 +6,363 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"errors"
 )
 
+type HaltError struct {
+}
+
+func (e HaltError) Error() string {
+	return fmt.Sprintf("Halted!")
+}
+type UnknownModeError struct {
+}
+
+func (e UnknownModeError) Error() string {
+	return fmt.Sprintf("Unknown Mode!")
+}
+
+type EmptyInputError struct{}
+
+func (e EmptyInputError) Error() string {
+	return fmt.Sprintf("Empty Input!")
+}
+
+type UnknownOpError struct {
+	op int
+}
+
+func (e UnknownOpError) Error() string {
+	return fmt.Sprintf("Unknown Operation:%v", e.op)
+}
+
+
 // Do the given operation on the tape
-func doOp(index int, tape []int) (bool, *int) {
-	op := tape[index]
-switch op {
-case 99:
-	return true, &tape[0]
-case 1:
-	loc1 := tape[index+1]
-	loc2 := tape[index+2]
-	loc3 := tape[index+3]
-	tape[loc3] = tape[loc1] + tape[loc2]
-	return false, nil
-case 2:
-	loc1 := tape[index+1]
-	loc2 := tape[index+2]
-	loc3 := tape[index+3]
-	tape[loc3] = tape[loc1] * tape[loc2]
-	return false, nil
-default:
-	return false, &tape[0]
-}
-}
-// printOp ...
-func printOp(index int, tape []int) (string,error) {
-	op := tape[index]
+func doOp(index *int, tape, input, output *[]int) (*int, error) {
+	splitOp:=func (op int) (int,[]int){
+		hundredThou:=op/100000
+		tenThou:=(op- hundredThou*100000)/10000
+		thou:=(op - hundredThou*100000-tenThou*10000)/1000
+		hun := (op - hundredThou*100000-tenThou*10000 - thou*1000)/100
+		res := (op - hundredThou*100000-tenThou*10000 - thou*1000 - hun*100)
+		return res,[]int{tenThou,thou,hun}
+	}
+	getValue:= func(loc ,mode int) (int ,error){
+		if mode ==0{
+			return (*tape)[loc],nil
+		}else if mode ==1{
+			return loc,nil
+		}else{
+			return 0, UnknownModeError{}
+		}
+	}
+
+
+	op,mode := splitOp((*tape)[*index])
 	switch op {
 	case 99:
-		return fmt.Sprintf("Halt\n"),nil
+		return &(*tape)[0], HaltError{}
+	case 1:
+		loc1 := (*tape)[*index+1]
+		loc2 := (*tape)[*index+2]
+		loc3 := (*tape)[*index+3]
+	        //fmt.Printf("Opcode:%v, mode:%v, loc1:%v, loc2:%v\n",op,mode[2], loc1,loc2)
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return nil,err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return nil,err
+		}
+		(*tape)[loc3] = val1 + val2
+		*index+=4
+		return nil, nil
+	case 2:
+		loc1 := (*tape)[*index+1]
+		loc2 := (*tape)[*index+2]
+		loc3 := (*tape)[*index+3]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return nil,err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return nil,err
+		}
+		(*tape)[loc3] = val1*val2
+		*index+=4
+		return nil, nil
+	case 3:
+		loc1 := (*tape)[*index+1]
+		if len(*input) > 0 {
+			(*tape)[loc1] = (*input)[0]
+			*index+=2
+			return nil, nil
+		} else {
+			return nil, EmptyInputError{}
+
+		}
+	case 4:
+		loc1 := (*tape)[*index+1]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return nil,err
+		}
+		*output = append(*output, val1)
+		*index+=2
+		return nil, nil
+	case 5:
+		loc1 := (*tape)[*index+1]
+		loc2 := (*tape)[*index+2]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return nil,err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return nil,err
+		}
+		if val1 !=0{
+			*index=val2
+		}else{
+			*index+=3
+		}
+		return nil, nil
+	case 6:
+		loc1 := (*tape)[*index+1]
+		loc2 := (*tape)[*index+2]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return nil,err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return nil,err
+		}
+		if val1 ==0{
+			*index=val2
+		}else{
+			*index+=3
+		}
+		return nil, nil
+	case 7:
+		loc1 := (*tape)[*index+1]
+		loc2 := (*tape)[*index+2]
+		loc3 := (*tape)[*index+3]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return nil,err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return nil,err
+		}
+		if val1<val2{
+			(*tape)[loc3]=1
+		}else {
+			(*tape)[loc3]=0
+		}
+		*index+=4
+		return nil, nil
+	case 8:
+		loc1 := (*tape)[*index+1]
+		loc2 := (*tape)[*index+2]
+		loc3 := (*tape)[*index+3]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return nil,err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return nil,err
+		}
+		if val1==val2{
+			(*tape)[loc3]=1
+		}else {
+			(*tape)[loc3]=0
+		}
+		*index+=4
+		return nil, nil
+	default:
+		return &(*tape)[0], UnknownOpError{op}
+	}
+}
+
+// printOp ...
+func printOp(index int, tape,input []int) (string, error) {
+	splitOp:=func (op int) (int,[]int){
+		hundredThou:=op/100000
+		tenThou:=(op- hundredThou*100000)/10000
+		thou:=(op - hundredThou*100000-tenThou*10000)/1000
+		hun := (op - hundredThou*100000-tenThou*10000 - thou*1000)/100
+		res := (op - hundredThou*100000-tenThou*10000 - thou*1000 - hun*100)
+		return res,[]int{tenThou,thou,hun}
+	}
+	getValue:= func(loc ,mode int) (int ,error){
+		if mode ==0{
+			return tape[loc],nil
+		}else if mode ==1{
+			return loc,nil
+		}else{
+			return 0, UnknownModeError{}
+		}
+	}
+
+
+	op,mode := splitOp(tape[index])
+	switch op {
+	case 99:
+		return fmt.Sprintf("Halt\n"), nil
 	case 1:
 		loc1 := tape[index+1]
 		loc2 := tape[index+2]
-		return fmt.Sprintf("Add:%d + %d = %d written to %d\n", tape[loc1],tape[loc2],tape[loc1] + tape[loc2],tape[index+3]),nil
+		loc3 := tape[index+3]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return "",err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return "",err
+		}
+		return fmt.Sprintf("Add:%d + %d = %d written to %d\n", val1, val2, val1+val2, loc3), nil
 	case 2:
 		loc1 := tape[index+1]
 		loc2 := tape[index+2]
-		return fmt.Sprintf("Mul:%d * %d =%d written to %d\n", tape[loc1],tape[loc2],tape[loc1] * tape[loc2],tape[index+3]),nil
+		loc3 := tape[index+3]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return "",err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return "",err
+		}
+		return fmt.Sprintf("Mul:%d + %d = %d written to %d\n", val1, val2, val1*val2, loc3), nil
+	case 3:
+		loc1 := tape[index+1]
+		return fmt.Sprintf("Input:%d written to %d\n",input[0],loc1 ), nil
+	case 4:
+		loc1 := tape[index+1]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return "",err
+		}
+		var s string
+		if mode[2]==1{
+			s="Imm"
+		} else{
+			s=strconv.Itoa(loc1)
+
+		}
+		return fmt.Sprintf("Output:%d from %v Written to output\n",val1,s), nil
+	case 5:
+		loc1 := tape[index+1]
+		loc2 := tape[index+2]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return "",err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return "",err
+		}
+		var s string
+		if val1 !=0{
+			s=fmt.Sprintf("Set to %d",val2)
+		}else{
+			s=fmt.Sprintf("Set to %d",index+3)
+		}
+		return fmt.Sprintf("JumpTrue:%v Inst -> %v\n", val1==1, s), nil
+	case 6:
+		loc1 := tape[index+1]
+		loc2 := tape[index+2]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return "",err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return "",err
+		}
+		var s string
+		if val1 ==0{
+			s=fmt.Sprintf("Set to %d",val2)
+		}else{
+			s=fmt.Sprintf("Set to %d",index+3)
+		}
+		return fmt.Sprintf("JumpTrue:%v Inst -> %v\n", val1==1, s), nil
+	case 7:
+		loc1 := tape[index+1]
+		loc2 := tape[index+2]
+		loc3 := tape[index+3]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return "",err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return "",err
+		}
+		return fmt.Sprintf("Less:%d < %d = %v written to %d\n", val1, val2, val1<val2, loc3), nil
+	case 8:
+		loc1 := tape[index+1]
+		loc2 := tape[index+2]
+		loc3 := tape[index+3]
+		val1,err:=getValue(loc1,mode[2])
+		if err!=nil {
+			return "",err
+		}
+		val2,err:=getValue(loc2,mode[1])
+		if err!=nil {
+			return "",err
+		}
+		return fmt.Sprintf("Equals:%d == %d = %v written to %d\n", val1, val2, val1==val2, loc3), nil
 	default:
-		return string(""), fmt.Errorf("Unknown Operation %d",op)
+		return string(""), fmt.Errorf("Unknown Operation %d", op)
 	}
 
 }
-// runTape Runs the current tape
-func runTape(tape []int) (int,error) {
-	finished := false
-	var result *int
-	for i := 0; finished != true; i += 4 {
-		// op,err := printOp(i,tape)
-		// if err != nil {
-		// 	return 0,fmt.Errorf("Failed to print operation:%w",err)
 
-		// }
-		//fmt.Printf(op)
-		finished,result = doOp(i, tape)
-		if !finished && result!=nil{
-			s,err:=printOp(i,tape)
+// runTape Runs the current tape
+func runTape(tape, input,output*[]int) (int, error) {
+	finished := false
+	for i := 0; finished != true;{
+
+
+		s,err:=printOp(i,*tape,*input)
+		fmt.Println(s)
+		fmt.Printf("return:%d\n",(*tape)[0])
+		if err!=nil{
 			return 0,fmt.Errorf("Invalid Operand at %d\n Tape:%v %w",i,s,err)
 		}
+		result,err := doOp(&i, tape,input,output)
+		if result!=nil{
+			fmt.Println(output)
+			return *result, nil
+		}
+		if err!=nil{
+			if errors.Is(err,HaltError{}){
+				finished=true
+			}else {
+				return 0, fmt.Errorf("Failed at %d\n Tape:%v %w", i, tape, err)
+			}
+		}
+
 	}
-	return *result,nil
+	return 0,fmt.Errorf("Unexpected Termination")
 }
 func main() {
 	fmt.Printf("hello, world\n")
-	data, err := ioutil.ReadFile("programs.txt")
+	data, err := ioutil.ReadFile("programs2.txt")
 	if err != nil {
 		fmt.Printf("File Error %w", err)
 		os.Exit(4)
 	}
 
 	program := strings.Split(strings.TrimSpace(string(data)), ",")
-	tape := make([]int,len(program))
+	tape := make([]int, len(program))
 	for i, s := range program {
 		tape[i], err = strconv.Atoi(s)
 		if err != nil {
@@ -85,22 +370,9 @@ func main() {
 			os.Exit(5)
 		}
 	}
-	for i:=0;i<100;i++{
-		for j:=0;j<100;j++{
-			working := make([]int,len(tape))
-			copy(working,tape)
-			working[1]=i
-			working[2]=j
-
-			result,err := runTape(working)
-			if err != nil {
-				fmt.Printf("Tape Error %w", err)
-				os.Exit(1)
-			}
-			if result == 19690720 {
-				fmt.Printf("Answer: %d",i*100+j)
-			}
-
-		}
+	_, err = runTape(&tape,&[]int{5},&[]int{})
+	if err != nil {
+		fmt.Printf("Tape Error %v", err)
+		os.Exit(1)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,67 +1,108 @@
 package main
 
 import "testing"
-import "github.com/google/go-cmp/cmp"
+import "reflect"
 
 func Test_doOp(t *testing.T) {
-	resultRet:=100
-	resultUnk:=101
+
 	type args struct {
-		index int
-		tape  []int
+		index  int
+		tape   *[]int
+		input  *[]int
+		output *[]int
 	}
 	tests := []struct {
-		name         string
-		args         args
-		wantFinished bool
-		wantResult   *int
-		wantTape []int
+		name     string
+		args     args
+		want     *int
+		wantErr  bool
+		wantTape *[]int
 	}{
-		{"Return",args{4,[]int{100,2,3,5,99}},true,&resultRet,[]int{100,2,3,5,99}},
-		{"Add",args{0,[]int{1,2,3,5,99,50,67,72}},false,nil,[]int{1,2,3,5,99,8,67,72}},
-		{"Multiply",args{0,[]int{2,2,3,5,99,50,67,72}},false,nil,[]int{2,2,3,5,99,15,67,72}},
-		{"Unknown Op",args{0,[]int{101,2,3,5,99}},false,&resultUnk,[]int{101,2,3,5,99}},
+		{"Return", args{4, &[]int{100, 2, 3, 5, 99}, &[]int{}, &[]int{}}, new(int),true, &[]int{100, 2, 3, 5, 99}},
+		{"Add", args{0, &[]int{1, 2, 3, 5, 99, 50, 67, 72}, &[]int{}, &[]int{}}, nil, false, &[]int{1, 2, 3, 5, 99, 8, 67, 72}},
+		{"Multiply", args{0, &[]int{2, 2, 3, 5, 99, 50, 67, 72}, &[]int{}, &[]int{}}, nil, false, &[]int{2, 2, 3, 5, 99, 15, 67, 72}},
 	}
+	*(tests[0].want)=100
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotFinished, gotResult := doOp(tt.args.index, tt.args.tape)
-			if gotFinished != tt.wantFinished {
-				t.Errorf("doOp() gotFinished = %v, want %v", gotFinished, tt.wantFinished)
+			got, err := doOp(&tt.args.index, tt.args.tape, tt.args.input, tt.args.output)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("doOp() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
-			if (gotResult ==nil || tt.wantResult==nil) && gotResult != tt.wantResult {
-				t.Errorf("doOp() gotResult = %v, want %v", gotResult, tt.wantResult)
-			}else if gotResult!=nil && *gotResult != *tt.wantResult {
-				t.Errorf("doOp() gotResult = %v, want %v", *gotResult, *tt.wantResult)
-			}
-			if diff := cmp.Diff(tt.args.tape, tt.wantTape);diff  != ""{
-				t.Errorf("doOp() Tape mismatch = %v",diff )
+			if !reflect.DeepEqual(got ,tt.want) {
+				t.Errorf("doOp() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
+func Test_printOp(t *testing.T) {
+	type args struct {
+		index int
+		tape  []int
+		input []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := printOp(tt.args.index, tt.args.tape, tt.args.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("printOp() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("printOp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func Test_runTape(t *testing.T) {
 	type args struct {
-		tape []int
+		tape *[]int
+		input  *[]int
+		output *[]int
 	}
 	tests := []struct {
 		name    string
 		args    args
 		want    int
 		wantErr bool
-		wantTape []int
+		wantTape *[]int
+		wantOutput *[]int
 	}{
-		{"Advent Test1",args{[]int{1,0,0,0,99}},2,false,[]int{2,0,0,0,99}},
-		{"Advent Test2",args{[]int{1,9,10,3,2,3,11,0,99,30,40,50}},3500,false,[]int{3500,9,10,70,2,3,11,0,99,30,40,50}},
-		{"Advent Test3",args{[]int{2,3,0,3,99}},2,false,[]int{2,3,0,6,99}},
-		{"Advent Test4",args{[]int{2,4,4,5,99,0}},2,false,[]int{2,4,4,5,99,9801}},
-		{"Advent Test5",args{[]int{1,1,1,4,99,5,6,0,99}},30,false,[]int{30,1,1,4,2,5,6,0,99}},
-		{"Advent Test Unknown",args{[]int{100,2,3,5,99}},0,true,[]int{100,2,3,5,99}},
+		// {"Advent Test1", args{&[]int{1, 0, 0, 0, 99}, &[]int{}, &[]int{}}, 2, false, &[]int{2, 0, 0, 0, 99},&[]int{}},
+		// {"Advent Test2", args{&[]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}, &[]int{}, &[]int{}}, 3500, false, &[]int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50},&[]int{}},
+		// {"Advent Test3", args{&[]int{2, 3, 0, 3, 99}, &[]int{}, &[]int{}}, 2, false, &[]int{2, 3, 0, 6, 99},&[]int{}},
+		// {"Advent Test4", args{&[]int{2, 4, 4, 5, 99, 0}, &[]int{}, &[]int{}}, 2, false, &[]int{2, 4, 4, 5, 99, 9801},&[]int{}},
+		// {"Advent Test5", args{&[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, &[]int{}, &[]int{}}, 30, false, &[]int{30, 1, 1, 4, 2, 5, 6, 0, 99},&[]int{}},
+		// {"Advent Test Unknown", args{&[]int{100, 2, 3, 5, 99}, &[]int{}, &[]int{}}, 0, true, &[]int{100, 2, 3, 5, 99},&[]int{}},
+		// {"Input Test Equal1", args{&[]int{3,9,8,9,10,9,4,9,99,-1,8}, &[]int{8}, &[]int{}}, 3, false, &[]int{3,9,8,9,10,9,4,9,99,1,8},&[]int{1}},
+		// {"Input Test Equal2", args{&[]int{3,9,8,9,10,9,4,9,99,-1,8}, &[]int{100}, &[]int{}}, 3, false, &[]int{3,9,8,9,10,9,4,9,99,0,8},&[]int{0}},
+		// {"Input Test Less Than 1", args{&[]int{3,9,7,9,10,9,4,9,99,-1,8}, &[]int{7}, &[]int{}}, 3, false, &[]int{3,9,7,9,10,9,4,9,99,1,8},&[]int{1}},
+		// {"Input Test Less Than 2", args{&[]int{3,9,7,9,10,9,4,9,99,-1,8}, &[]int{100}, &[]int{}}, 3, false, &[]int{3,9,7,9,10,9,4,9,99,0,8},&[]int{0}},
+		// {"Input Test Imm Equal1", args{&[]int{3,3,1108,-1,8,3,4,3,99}, &[]int{8}, &[]int{}}, 3, false, &[]int{3,3,1108,1,8,3,4,3,99},&[]int{1}},
+		// {"Input Test Imm Equal2", args{&[]int{3,3,1108,-1,8,3,4,3,99}, &[]int{100}, &[]int{}}, 3, false, &[]int{3,3,1108,0,8,3,4,3,99},&[]int{0}},
+		// {"Input Test Imm Less Than 1", args{&[]int{3,3,1107,-1,8,3,4,3,99}, &[]int{7}, &[]int{}}, 3, false, &[]int{3,3,1107,1,8,3,4,3,99},&[]int{1}},
+		// {"Input Test Imm Less Than 2", args{&[]int{3,3,1107,-1,8,3,4,3,99}, &[]int{100}, &[]int{}}, 3, false, &[]int{3,3,1107,0,8,3,4,3,99},&[]int{0}},
+
+		// {"Xor ", args{&[]int{3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9}, &[]int{100}, &[]int{}}, 3, false, &[]int{3,12,6,12,15,1,13,14,13,4,13,99,100,1,1,9},&[]int{1}},
+		// {"Xor 2 ", args{&[]int{3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9}, &[]int{0}, &[]int{}}, 3, false, &[]int{3,12,6,12,15,1,13,14,13,4,13,99,0,0,1,9},&[]int{0}},
+		{"Xor Imm ", args{&[]int{3,3,1105,-1,9,1101,0,0,12,4,12,99,1}, &[]int{100}, &[]int{}}, 3, false, &[]int{3,3,1105,100,9,1101,0,0,12,4,12,99,0},&[]int{1}},
+		// {"Xor Imm 2", args{&[]int{3,3,1105,-1,9,1101,0,0,12,4,12,99,1}, &[]int{0}, &[]int{}}, 3, false, &[]int{3,3,1105,0,9,1101,0,0,12,4,12,99,0},&[]int{0}},
+
 	}
-	for _, tt := range tests  {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := runTape(tt.args.tape)
+			got, err := runTape(tt.args.tape,tt.args.input,tt.args.output)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("runTape() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -69,8 +110,11 @@ func Test_runTape(t *testing.T) {
 			if got != tt.want {
 				t.Errorf("runTape() = %v, want %v", got, tt.want)
 			}
-			if diff := cmp.Diff(tt.args.tape, tt.wantTape);diff  != ""{
-				t.Errorf("doOp() Tape mismatch = %v",diff )
+			if !reflect.DeepEqual(tt.args.tape ,tt.wantTape) {
+				t.Errorf("doOp() = %v, want %v", tt.args.tape , tt.wantTape)
+			}
+			if !reflect.DeepEqual(tt.args.output ,tt.wantOutput) {
+				t.Errorf("doOp() = %v, want %v", tt.args.output, tt.wantOutput)
 			}
 		})
 	}
